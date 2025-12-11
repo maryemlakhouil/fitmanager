9742e4da873b569acabbd379f01d772c74166989
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-12-11 16:56:25 +0100

    fichier index.php qui dereger user vers la page principale

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
 <?php
 session_start();
 
-// Si l'utilisateur est déjà connecté → redirection vers dashboard
+// Si l'utilisateur est déjà connecté : redirection vers dashboard
 if (isset($_SESSION['user_id'])) {
     header("Location: dashbord.php");
     exit;
 }
 
-// Sinon → redirection vers la page login
+// Sinon :redirection vers la page login
 header("Location: login.php");
 exit;
 ?>
