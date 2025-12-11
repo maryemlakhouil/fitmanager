cc2f981a3b3da70ad3219143268e37d5967d8ed2
maryemlakhouil <lkhwilmaryem@gmail.com>
2025-12-11 17:41:51 +0100

    structurer le code sql version 1

--- database.sql
+++ database.sql
@@ -41,6 +41,14 @@ create table cours_equipements(
     constraint fk_equipement foreign key (equipe_ID) references equipements(equipe_ID) on delete CASCADE,
     unique (courId,equipe_ID)
 );
+/*Céation Du table Users Pour Authentification */
+
+create table users(
+    id int auto_increment Primary key,
+    nom varchar(50) Not null,
+    email varchar(50) unique ,
+    password varchar(50) not null
+);
 
 /* Lister les Données */
 
@@ -49,6 +57,7 @@ select * from equipements;
 select * from cours_equipements;
 select * from cours order by dateDebut desc;
 SELECT * FROM equipements ORDER BY nom DESC;
+select * from users;
 
 /***************************
         INSERTION  
@@ -65,61 +74,54 @@ INSERT INTO equipements (nom, type, quantite, etat) VALUES ("equipe2","Haltères
 INSERT INTO equipements (nom, type, quantite, etat) VALUES ("equipe3","Ballons", 17, "moyen")
 INSERT INTO equipements (nom, type, quantite, etat) VALUES ("equipe4","Ballons", 20, "bon")
 
+/* Inserer Dans La table Users*/
+insert into users(nom,email,password) values('maryem','lkhwil@gmail.com','lakhouil2003');
 
+/**************************
+    Supprimer Les Données
+**************************/
 
 /*Supprimer Un cours du tableau cours*/
 delete from cours where courId = 6;
+
+/* Suprimer Un Equipements */
+DELETE FROM equipements where equipe_ID=1;
+
+/*******************************
+    Modifier les Données 
+******************************/
 
 /*Modifier Un cours */
 Update cours
     SET nom="sience", categorie="Yoga", nbmax=30
     WHERE courId=7;
 
-
 /*Modifier Un equipements */
-
 Update equipements
     SET type="Ballons",etat="moyen"
     WHERE equipe_ID=1;
 
-/* Suprimer Un Equipements */
-DELETE FROM equipements where equipe_ID=1;
+/* Modifier le colonne Password */
+alter table users modify password varchar(255);
+
+/*****************************
+    Récuperer Les Données 
+*****************************/
 
 /*Récupération des cours par catégorie*/
-
 SELECT categorie, COUNT(*) AS total
 FROM cours
 WHERE categorie IS NOT NULL
 GROUP BY categorie;
 
 /* Récuperation ds equipements par leur type */
-
 SELECT type, COUNT(*) AS total 
 FROM equipements
 WHERE type is not null 
 GROUP BY type;
 
 /* Le nombre de categorie de cours */
-
 SELECT COUNT(DISTINCT categorie) AS nb_categories
 FROM cours;
 
-/*Céation Du table Users Pour Authentification */
 
-create table users(
-    id int auto_increment Primary key,
-    nom varchar(50) Not null,
-    email varchar(50) unique ,
-    password varchar(50) not null
-);
-/* Modifier le colonne Password */
-
-alter table users modify password varchar(255);
-
-/* Inserer Dans La table Users*/
-
-insert into users(nom,email,password) values('maryem','lkhwil@gmail.com','lakhouil2003');
-
-/*Lister les users */
-
-select * from users;
